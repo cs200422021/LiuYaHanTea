e721a575d546139f4e561390bea08451e4ad6a89
cs200422021 <442632374@qq.com>
2016-04-12 09:26:16 +0800

    tabfragment update

--- app/src/main/java/com/tea/liuyahan/liuyahantea/TabFragment.java
+++ app/src/main/java/com/tea/liuyahan/liuyahantea/TabFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
 public class TabFragment extends Fragment {
     private String tab;
     private TextView tabTextView;
+    private ListView newsListView;
 
     public static TabFragment newInstance(String tab) {
         Bundle args = new Bundle();
@@ -30,6 +32,8 @@ public class TabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
         tabTextView = (TextView) rootView.findViewById(R.id.tab_tv);
+        newsListView = (ListView) rootView.findViewById(R.id.news_list_view);
+        newsListView.setEmptyView(tabTextView);
         tabTextView.setText(tab);
         return rootView;
     }
