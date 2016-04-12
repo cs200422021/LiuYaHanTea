38765ab85b72ce7d1ed3dd418d9881357967bd5c
cs200422021 <442632374@qq.com>
2016-04-12 10:48:55 +0800

    donwload json successfully update

---
+++ app/src/main/java/com/tea/liuyahan/liuyahantea/adapters/NewsFragmentAdapter.java
@@ -0,0 +1,101 @@
+package com.tea.liuyahan.liuyahantea.adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tea.liuyahan.liuyahantea.R;
+import com.tea.liuyahan.liuyahantea.entity.TeaInfoEntity;
+import com.tea.liuyahan.liuyahantea.utils.MyHttpUtils;
+
+import java.util.List;
+
+public class NewsFragmentAdapter extends BaseAdapter {
+    private List<TeaInfoEntity.DataEntity> datas;
+    private LayoutInflater inflater;
+
+    // 主线程中的Handler对象
+    private Handler handler;
+
+    public NewsFragmentAdapter(Context context, List<TeaInfoEntity.DataEntity> datas, Handler handler) {
+        this.datas = datas;
+        inflater = LayoutInflater.from(context);
+        this.handler = handler;
+    }
+
+    @Override
+    public int getCount() {
+        return datas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.layout_item_fragment_tab, parent, false);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.titleTextView.setText(datas.get(position).getTitle());
+        if (datas.get(position).getDescription()!=null){
+            holder.descTextView.setText(datas.get(position).getDescription());
+        }else {
+            holder.descTextView.setText(" ");
+        }
+        holder.infosTextView.setText(datas.get(position).getSource()+"  "+datas.get(position).getNickname()+"  "+datas.get(position).getCreate_time());
+        // 异步加载图片
+        String imgUrl = datas.get(position).getWap_thumb();
+        downloadImage(imgUrl, holder.iconImageView);
+        return convertView;
+    }
+
+    // 下载网络图片
+    private void downloadImage(final String url, final ImageView iconImageView) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final Bitmap bitmap = MyHttpUtils.getBitmapFromUrl(url);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        iconImageView.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private class ViewHolder {
+        ImageView iconImageView;
+        TextView titleTextView;
+        TextView descTextView;
+        TextView infosTextView;
+
+        public ViewHolder(View convertView) {
+            iconImageView = (ImageView) convertView.findViewById(R.id.icon_iv);
+            titleTextView = (TextView) convertView.findViewById(R.id.title_tv);
+            descTextView = (TextView) convertView.findViewById(R.id.desc_tv);
+            infosTextView = (TextView) convertView.findViewById(R.id.infos_tv);
+        }
+    }
+
+}
--- app/src/main/java/com/tea/liuyahan/liuyahantea/fragments/TabFragment.java
+++ app/src/main/java/com/tea/liuyahan/liuyahantea/fragments/TabFragment.java
@@ -1,6 +1,8 @@
 package com.tea.liuyahan.liuyahantea.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +10,14 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.tea.liuyahan.liuyahantea.R;
+import com.tea.liuyahan.liuyahantea.adapters.NewsFragmentAdapter;
+import com.tea.liuyahan.liuyahantea.entity.TeaInfoEntity;
+import com.tea.liuyahan.liuyahantea.utils.MyHttpUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TabFragment extends Fragment {
@@ -23,6 +32,19 @@ public class TabFragment extends Fragment {
     public static final String HOME_URL = BASE_URL + "news.getSlideshow";
     //头条数据
     public static final String HEADLINE_URL = BASE_URL + "news.getHeadlines";
+    //接收数据的
+    private List<TeaInfoEntity.DataEntity> datas;
+    private NewsFragmentAdapter adapter;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            List<TeaInfoEntity.DataEntity> tempDatas = (List<TeaInfoEntity.DataEntity>) msg.obj;
+            datas.addAll(tempDatas);
+            adapter.notifyDataSetChanged();
+        }
+    };
 
 
     public static TabFragment newInstance(String tab) {
@@ -37,6 +59,7 @@ public class TabFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tab = getArguments().getString("tab");
+
     }
 
     @Override
@@ -46,6 +69,31 @@ public class TabFragment extends Fragment {
         newsListView = (ListView) rootView.findViewById(R.id.news_list_view);
         newsListView.setEmptyView(tabTextView);
         tabTextView.setText(tab);
+        // 绑定适配器
+        datas = new ArrayList<>();
+        adapter = new NewsFragmentAdapter(getActivity(), datas, handler);
+        newsListView.setAdapter(adapter);
+
+        // 下载网络数据
+        downloadJson(HEADLINE_URL+PAGE+"1");
         return rootView;
+    }
+
+    // 开启子线程下载数据
+    private void downloadJson(final String url) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String json = MyHttpUtils.getTextFromUrl(url);
+                Gson gson = new Gson();
+                TeaInfoEntity teaInfoEntity = gson.fromJson(json, TeaInfoEntity.class);
+                List<TeaInfoEntity.DataEntity> datas = teaInfoEntity.getData();
+
+                // 将数据发送至主线程并处理
+                Message message = Message.obtain();
+                message.obj = datas;
+                handler.sendMessage(message);
+            }
+        }).start();
     }
 }
