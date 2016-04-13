1620d5acce3339ba7df43145f49a17f99acc43fb
cs200422021 <442632374@qq.com>
2016-04-13 20:39:00 +0800

    appbarlayout update

--- app/src/main/java/com/tea/liuyahan/liuyahantea/activity/News_Web_ViewActivity.java
+++ app/src/main/java/com/tea/liuyahan/liuyahantea/activity/News_Web_ViewActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.tea.liuyahan.liuyahantea.R;
@@ -25,6 +28,9 @@ public class News_Web_ViewActivity extends AppCompatActivity {
     //接收点击时通过intent传过来的url；
     private String newsUrl;
 
+    private Toolbar toolbar;
+    private TextView appBarTextView;
+
     //接收数据的
     private List<NewsEntity.DataEntity> datas;
     private Handler handler = new Handler() {
@@ -35,7 +41,7 @@ public class News_Web_ViewActivity extends AppCompatActivity {
             datas.addAll(tempDatas);
             // 加载HTML5代码
             newsWebView.loadDataWithBaseURL(null, datas.get(0).getWap_content(), "text/html", "UTF-8", null);
-
+            appBarTextView.setText(datas.get(0).getTitle() + "\n" + datas.get(0).getAuthor() + "  " + datas.get(0).getCreate_time());
         }
     };
 
@@ -43,6 +49,12 @@ public class News_Web_ViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news__web__view);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        appBarTextView = (TextView) findViewById(R.id.time_author_tv);
+        ActionBar actionBar = getSupportActionBar();
+        setSupportActionBar(toolbar);
+
         newsWebView = (WebView) findViewById(R.id.news_web_view);
 
         // 设置内嵌app中WebView客户端
@@ -77,5 +89,8 @@ public class News_Web_ViewActivity extends AppCompatActivity {
 
     public void saveAll(View view){
         Snackbar.make(view,"假装收藏了，稍后添加功能",Snackbar.LENGTH_SHORT).show();
+    }
+    public void closeWebView(View view){
+        finish();
     }
 }
